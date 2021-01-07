0ca894840f8251eed41c2c8ae7d5c3dbe50e932b
andre saavedra <andre.saavedra@hotmail.com>
2021-01-07 16:15:51 +0000

    melhorei a barra de navegação para dispositivos móveis

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,18 @@
 var menu_icon = document.getElementById("menu");
 var menu_links = document.getElementById("dropdown");
+var menu_cancelar = document.getElementById("cancelar");
 
 
 
 
 menu_icon.onclick = () => {
     menu_links.setAttribute("style", "display:flex;");
+    menu_icon.setAttribute("style", "display: none;");
+    menu_cancelar.setAttribute("style", "display: block;");
+};
+
+menu_cancelar.onclick = () => {
+    menu_links.setAttribute("style", "display:none;");
+    menu_icon.setAttribute("style", "display: block;");
+    menu_cancelar.setAttribute("style", "display: none;");
 };
