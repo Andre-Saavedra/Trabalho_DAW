771616ae499373e80226e82f10f1d768051f7f7b
andre saavedra <andre.saavedra@hotmail.com>
2021-01-08 13:01:24 +0000

    pequenas alterações no código

--- js/menu.js
+++ js/menu.js
@@ -6,13 +6,15 @@ var menu_cancelar = document.getElementById("cancelar");
 
 
 menu_icon.onclick = () => {
-    menu_links.setAttribute("style", "display:flex;");
+    //menu_links.setAttribute("style", "display:flex;");
+    menu_links.setAttribute("style", "opacity:1;");
     menu_icon.setAttribute("style", "display: none;");
     menu_cancelar.setAttribute("style", "display: block;");
 };
 
 menu_cancelar.onclick = () => {
-    menu_links.setAttribute("style", "display:none;");
+    //menu_links.setAttribute("style", "display:none;");
+    menu_links.setAttribute("style", "opacity:0;");
     menu_icon.setAttribute("style", "display: block;");
     menu_cancelar.setAttribute("style", "display: none;");
 };
